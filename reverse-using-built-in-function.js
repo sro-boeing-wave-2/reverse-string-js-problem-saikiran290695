5945f4e0a4904e93cde170a1ea5674178d941a22
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-14 21:30:55 +0530

    arrow function modified

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,8 +1,5 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (text) => {
-  const response = text.split('').reverse().join(''); //  inbuilt functions to reverse a string
-  return response;
-};
+const reverse = text => (text.split('').reverse().join('')); //  inbuilt functions to reverse a string
 
 module.exports = reverse;
